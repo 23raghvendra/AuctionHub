ef660ba082a2d0fb1c964902595e914623fb0b98
Raghvendra Singh <2006raghvendrasingh@gmail.com>
2025-12-04 15:11:02 +0530

    backend url update

--- frontend/src/store/slices/commissionSlice.js
+++ frontend/src/store/slices/commissionSlice.js
@@ -24,7 +24,7 @@ export const postCommissionProof = (data) => async (dispatch) => {
   dispatch(commissionSlice.actions.postCommissionProofRequest());
   try {
     const response = await axios.post(
-      "http://localhost:8080/api/v1/commission/proof",
+      "https://auctionhub-mybe.onrender.com/api/v1/commission/proof",
       data,
       {
         withCredentials: true,
